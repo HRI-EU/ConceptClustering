9e5435276cbd9168dd0e00048233d77195455a35
Felix Lanfermann <felix.lanfermann@honda-ri.de>
2024-07-23 14:47:03 +0200

    add pkgInfo.py

---
+++ pkgInfo.py
@@ -0,0 +1,26 @@
+#
+#  Custom package settings
+#
+#  Copyright (C)
+#  Honda Research Institute Europe GmbH
+#  Carl-Legien-Str. 30
+#  63073 Offenbach/Main
+#  Germany
+#
+#  UNPUBLISHED PROPRIETARY MATERIAL.
+#  ALL RIGHTS RESERVED.
+#
+#
+
+
+name = 'concept_clustering'
+
+version = '0.1'
+
+category = 'Libraries'
+
+envVars = [('PYTHONPATH', '${INSTALL_ROOT}/include:${PYTHONPATH}')]
+
+sqLevel = "basic"
+
+sqOptOutRules = ["GEN03"]
