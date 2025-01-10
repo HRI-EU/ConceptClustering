d30e59b5c9ce6a4aac1533997d4d6c5346f8a0a6
Felix Lanfermann <felix.lanfermann@honda-ri.de>
2025-01-10 15:19:10 +0100

    add two tests

--- test/test_clustering.py
+++ test/test_clustering.py
@@ -19,6 +19,7 @@ import pytest
 import unittest
 
 import numpy as np
+import pandas as pd
 
 from concept_clustering.concept_clustering import ConceptClustering
 
@@ -38,18 +39,34 @@ def test_f3():
     assert c == 5
 
 
-def test_check_parames_max_iter():
+def test_check_params_max_iter():
     with pytest.raises(ValueError):
-        ConceptClustering(
-            description_spaces=None, n_clusters=3, max_iter=-50,
-        ).fit(X=None, centers=None)
+        ConceptClustering(description_spaces=None, n_clusters=3, max_iter=-50,).fit(
+            X=None, centers=None
+        )
 
 
-def test_check_parames_n_clusters():
+def test_check_params_n_clusters():
     with pytest.raises(ValueError):
-        ConceptClustering(
-            description_spaces=None, n_clusters=-3, max_iter=50,
-        ).fit(X=None, centers=None)
+        ConceptClustering(description_spaces=None, n_clusters=-3, max_iter=50,).fit(
+            X=None, centers=None
+        )
+
+
+def test_check_params_n_clusters_x_shape():
+    data = pd.DataFrame(np.array([[1, 2], [4, 5]]), columns=["a", "b"])
+    with pytest.raises(ValueError):
+        ConceptClustering(description_spaces=None, n_clusters=3, max_iter=50,).fit(
+            X=data, centers=None
+        )
+
+
+def test_check_params_x_type():
+    data = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    with pytest.raises(TypeError):
+        ConceptClustering(description_spaces=None, n_clusters=3, max_iter=50,).fit(
+            X=data, centers=None
+        )
 
 
 if __name__ == "__main__":
